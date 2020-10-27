b951fb6fc9e59518eb42f6ead16941866113fae2
Nil <nilpatel111296@gmail.com>
2020-10-27 14:07:15 -0500

    Initial Commit

---
+++ src/main/java/com/photoapp/discovery/PhotoappDiscoveryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.photoapp.discovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotoappDiscoveryServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhotoappDiscoveryServiceApplication.class, args);
+    }
+
+}
